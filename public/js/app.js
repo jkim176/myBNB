56742be04ed3256c1ace3fcb3be17453d6f5a1ff
jk <chriskim4747@gmail.com>
2020-01-29 10:48:41 -0500

    added key events on form in Availability

--- public/js/app.js
+++ public/js/app.js
@@ -1989,12 +1989,19 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
       from: null,
       to: null
     };
+  },
+  methods: {
+    check: function check() {
+      alert("This will be checked!");
+    }
   }
 });
 
@@ -38268,6 +38275,15 @@ var render = function() {
           attrs: { type: "text", name: "from", placeholder: "Start date" },
           domProps: { value: _vm.from },
           on: {
+            keyup: function($event) {
+              if (
+                !$event.type.indexOf("key") &&
+                _vm._k($event.keyCode, "enter", 13, $event.key, "Enter")
+              ) {
+                return null
+              }
+              return _vm.check($event)
+            },
             input: function($event) {
               if ($event.target.composing) {
                 return
@@ -38294,6 +38310,15 @@ var render = function() {
           attrs: { type: "text", name: "to", placeholder: "End date" },
           domProps: { value: _vm.to },
           on: {
+            keyup: function($event) {
+              if (
+                !$event.type.indexOf("key") &&
+                _vm._k($event.keyCode, "enter", 13, $event.key, "Enter")
+              ) {
+                return null
+              }
+              return _vm.check($event)
+            },
             input: function($event) {
               if ($event.target.composing) {
                 return
@@ -38305,9 +38330,11 @@ var render = function() {
       ])
     ]),
     _vm._v(" "),
-    _c("button", { staticClass: "btn btn-secondary btn-block" }, [
-      _vm._v("Check!")
-    ])
+    _c(
+      "button",
+      { staticClass: "btn btn-secondary btn-block", on: { click: _vm.check } },
+      [_vm._v("Check!")]
+    )
   ])
 }
 var staticRenderFns = []
