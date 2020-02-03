1a23946c19b840b68f98f41df069885e67fd022c
jk <chriskim4747@gmail.com>
2020-02-03 14:42:10 -0500

    added BookableAvailabilityController

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('bookables', 'Api\BookableController@index');
 Route::get('bookables/{id}', 'Api\BookableController@show');
 
+Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')
+    ->name('bookables.availability.show');
+
 //Route::apiResource('bookables', 'Api\BookableController')
 //    ->only(['index', 'show']);
