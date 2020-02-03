e6a46e97163de7d122cea8799da69fbca40efc3b
jk <chriskim4747@gmail.com>
2020-02-03 17:08:31 -0500

    added Booking Availability validation on DB

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Bookable;
+
 class BookableAvailabilityController extends Controller
 {
     /**
@@ -13,11 +15,14 @@ class BookableAvailabilityController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke($id, Request $request)
     {
         $data = $request->validate([
             'from' => 'required|date_format:Y-m-d|after_or_equal:now',
             'to' => 'required|date_format:Y-m-d|after_or_equal:from'
         ]);
+
+        $bookable = Bookable::findOrFail($id);
+        dd($bookable->bookings()->betweenDates($data['from'], $data['to'])->count());
     }
 }
