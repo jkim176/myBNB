de542dc98c4398e30a8d3e9bd34e93a23f8ca113
jk <chriskim4747@gmail.com>
2020-02-04 09:23:42 -0500

    added Bookable Availability HTTP responses

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -23,6 +23,9 @@ class BookableAvailabilityController extends Controller
         ]);
 
         $bookable = Bookable::findOrFail($id);
-        dd($bookable->bookings()->betweenDates($data['from'], $data['to'])->count());
+        //dd($bookable->bookings()->betweenDates($data['from'], $data['to'])->count());
+        return $bookable->availableFor($data['from'], $data['to'])
+            ? response()->json([])
+            : response()->json([], 404);
     }
 }
