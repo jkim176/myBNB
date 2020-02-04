28b553e452f77685e7b1379a5643355256605c39
jk <chriskim4747@gmail.com>
2020-02-04 10:55:22 -0500

    added Availability.vue form validation on DB

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -11,8 +11,16 @@
                     class="form-control form-control-sm"
                     placeholder="Start date"
                     v-model="from"
-                    @keyup.enter="check"
+                    @keyup.enter="checkAvailability"
+                    :class="[{'is-invalid': this.errorFor('from')}]"
                 />
+                <div
+                    class="invalid-feedback"
+                    v-for="(error, index) in this.errorFor('from')"
+                    :key="'from' + index"
+                >
+                    {{ error }}
+                </div>
             </div>
 
             <div class="form-group col-md-6">
@@ -23,12 +31,25 @@
                     class="form-control form-control-sm"
                     placeholder="End date"
                     v-model="to"
-                    @keyup.enter="check"
+                    @keyup.enter="checkAvailability"
+                    :class="[{'is-invalid': this.errorFor('to')}]"
                 />
+                <div
+                    class="invalid-feedback"
+                    v-for="(error, index) in this.errorFor('to')"
+                    :key="'to' + index"
+                >
+                    {{ error }}
+                </div>
             </div>
         </div>
 
-        <button class="btn btn-secondary btn-block" @click="check">Check!</button>
+        <button class="btn btn-secondary btn-block"
+            @click="checkAvailability"
+            :disabled="loading"
+        >
+            Check!
+        </button>
     </div>
 </template>
 
@@ -38,12 +59,44 @@
             return {
                 from: null,
                 to: null,
+                loading: false,
+                status: null,
+                errors: null
             };
         },
         methods: {
-            check() {
-                alert("This will be checked!");
+            checkAvailability() {
+                this.loading = true;
+                this.errors = null;
+
+                axios.get(`/api/bookables/${this.$route.params.id}/availability?from=${this.from}&to=${this.to}`)
+                    .then(response => {
+                        this.status = response.status;
+                    }).catch(error => {
+                        if (422 === error.response.status) {
+                            this.errors = error.response.data.errors;
+                        }
+                        this.status = error.response.status;
+                    }).then(() => {
+                        this.loading = false;
+                    });
             },
+            errorFor(field) {
+                return this.hasErrors && this.errors[field]
+                    ? this.errors[field]
+                    : null;
+            }
+        },
+        computed: {
+            hasErrors() {
+                return 422 === this.status && this.errors != null;
+            },
+            isAvailable() {
+                return 200 === this.status;
+            },
+            notAvailable() {
+                return 400 === this.status;
+            }
         }
     }
 </script>
@@ -54,5 +107,14 @@
         text-transform: uppercase;
         color: gray;
         font-weight: bolder;
+    }
+
+    .is-invalid {
+        border-color: #b22222;
+        background-image: none;
+    }
+
+    .invalid-feedback {
+        color: #b22222;
     }
 </style>
